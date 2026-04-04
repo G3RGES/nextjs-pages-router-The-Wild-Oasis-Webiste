0b36f8474862cc17db5410e872fb2574cecbe7d6
G3RGES <georgios.nashaat@gmail.com>
2026-04-04 12:01:55 +0200

    Update [cabinId].js

--- pages/cabins/[cabinId].js
+++ pages/cabins/[cabinId].js
@@ -1,16 +1,31 @@
+import CabinView from "@/components/CabinView";
+import { getCabin } from "@/lib/data-service";
 import Head from "next/head";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
-function Cabin() {
-  const router = useRouter();
+//* DYNAMICALLY GENERATED (SSR)
+export async function getServerSideProps({ params }) {
+  const cabin = await getCabin(params.cabinId);
+
+  return { props: { cabin } };
+}
+
+//* SSG getStaticProps + getStaticPaths
+
+function Cabin({ cabin }) {
+  // const router = useRouter();
+  //* console.log(cabin);
 
   return (
     <>
       <Head>
-        <title>The Wild Oasis - Cabin {router.query.cabinId}</title>
+        {/* <title>The Wild Oasis - Cabin {router.query.cabinId}</title> */}
+        <title>The Wild Oasis - Cabin {cabin.name}</title>
       </Head>
 
-      <div>cabin #{router.query.cabinId}</div>
+      <div className="max-w-6xl mx-auto mt-8">
+        <CabinView cabin={cabin} />
+      </div>
     </>
   );
 }
