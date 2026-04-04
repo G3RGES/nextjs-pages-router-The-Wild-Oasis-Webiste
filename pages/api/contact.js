0fb7c40c64eb57ae0fd31090cf39c0e3e38e466c
G3RGES <georgios.nashaat@gmail.com>
2026-04-04 13:06:47 +0200

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,3 +1,25 @@
+import { supabase } from "@/lib/supabase";
+
 export default async function handler(req, res) {
-  res.status(200).json({ message: "Message received successfully!" });
+  const contactData = {
+    fullName: "Gerges Nashaat",
+    email: "georgios.nashaat@test.com",
+    subject: "test",
+    message: "test",
+  };
+
+  const { error } = await supabase.from("contact").insert([contactData]);
+
+  if (error)
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Failed to send message. Please try again",
+      });
+
+  //* SUCCESS MESSAGE
+  res
+    .status(200)
+    .json({ success: true, message: "Message received successfully!" });
 }
